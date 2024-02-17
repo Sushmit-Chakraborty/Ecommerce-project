696f29e216eb4174e96cf25cf90ede083d08c0ab
Sushmit <sushmit.chakrobarty@gmail.com>
2024-02-17 21:36:42 +0530

    Updated cart and checkout page

--- store/models.py
+++ store/models.py
@@ -47,6 +47,15 @@ class Order(models.Model):
         orderitems = self.orderitem_set.all()
         total = sum([item.quantity for item in orderitems])
         return total
+    
+    @property
+    def shipping(self):
+        shipping = False
+        orderItems = self.orderitem_set.all()
+        for i in orderItems:
+            if i.product.digital == False:
+                shipping = True
+        return shipping
 
 class OrderItem(models.Model):
     product = models.ForeignKey(Product,on_delete=models.SET_NULL,blank=True,null=True)
